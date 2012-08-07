8790bc2c63a68d6fa0efd3bc85906c8bb1d3b0e0
Nathan Handler <nhandler@ubuntu.com>
2012-08-07 12:47:16 -0500

    Add initial irssi script

---
+++ devspot.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use Irssi;
+use Irssi::Irc;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "1.0";
+%IRSSI = (
+    authors     =>      'Nathan Handler',
+    contact     =>      'nhandler@ubuntu.com',
+    name        =>      'devspot',
+    description =>      'Easily Spot Developers on IRC in irssi',
+    license     =>      'GPLv3+',
+);
+
+my %names = ();
+
+&reloadList();
+
+sub reloadList {
+    my $path = Irssi::settings_get_str('devspot_list');
+    %names = ();
+    open(NAMES, "<$path");
+    my @NAMES = <NAMES>;
+    close(NAMES);
+    foreach my $line (@NAMES) {
+        my($nick,$name) = split(/ /, $line,  2);
+        $names{lc($nick)}=$name;
+    }}
+}
+
+sub isdev {
+    my($data,$server,$witem) = @_;
+    &reloadList();
+    if($names{lc($data)}) {
+        Irssi::print("$data is a developer (" . $names{lc($data)} . ")");
+    }
+    else {
+        Irssi::print("$data is NOT a developer");
+    }
+}
+
+Irssi::settings_add_str('devspot', 'devspot_list', '~/.irssi/devspot.list');
+Irssi::command_bind('isdev', 'isdev');
